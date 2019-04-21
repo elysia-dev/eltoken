c8bba579105a550c8a1b3b5f31e83bad3e3a7a42
Elysia-HQ <junggun.lim@elysia.or.kr>
2019-04-21 20:08:56 +0900

    deploy test

--- truffle-config.js
+++ truffle-config.js
@@ -17,12 +17,14 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
+const fs = require('fs');
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "3d0e0bddf53e425da13312c824e13786";
+const infuraKey_ropsten = "3d0e0bddf53e425da13312c824e13786";
+const infuraKey_mainnet = fs.readFileSync(".infurakey").toString();
 
-const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic_test = fs.readFileSync(".secret_test").toString().trim();
 
 module.exports = {
   /**
@@ -62,13 +64,20 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+      provider: () => new HDWalletProvider(mnemonic_test, `https://ropsten.infura.io/${infuraKey_ropsten}`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/${infuraKey_mainnet}`),
+      network_id: 1,       // Mainnet's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gasPrice: 3000000000
+    }
 
     // Useful for private networks
     // private: {
@@ -88,13 +97,13 @@ module.exports = {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      },
       //  evmVersion: "byzantium"
-      // }
     }
   }
 }
